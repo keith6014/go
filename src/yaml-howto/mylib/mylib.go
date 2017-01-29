51cf0841842858ce09756314b4a1e96848676565
user <keith6014@gmail.com>
2017-01-29 15:34:05 -0500

    library support

--- src/yaml-howto/mylib/mylib.go
+++ src/yaml-howto/mylib/mylib.go
@@ -1,7 +1,22 @@
 package mylib
 
-import ()
+import (
+	"log"
+)
 
 func A() bool {
+	return true
+}
+
+type Config struct {
+	Info string
+}
+
+func ValidateConfig(myconfig *Config) bool {
+
+	if len(myconfig.Info) == 0 {
+		log.Fatal("info is not set")
+	}
+
 	return true
 }
--- src/yaml-howto/yaml-howto.go
+++ src/yaml-howto/yaml-howto.go
@@ -8,28 +8,15 @@ import (
 	"yaml-howto/mylib"
 )
 
-type Config struct {
-	Info string
-}
-
-func validateConfig(myconfig *Config) bool {
-
-	if len(myconfig.Info) == 0 {
-		log.Fatal("info is not set")
-	}
-
-	return true
-}
-
 func main() {
 	reader, _ := os.Open("data/data.yaml")
 	buf, _ := ioutil.ReadAll(reader)
 
 	log.Printf("%v\n", mylib.A())
-	var conf Config
+	var conf mylib.Config
 	yaml.Unmarshal(buf, &conf)
 
-	log.Printf("%v\n", validateConfig(&conf))
+	log.Printf("%v\n", mylib.ValidateConfig(&conf))
 
 	log.Printf("%v\n", conf)
 
